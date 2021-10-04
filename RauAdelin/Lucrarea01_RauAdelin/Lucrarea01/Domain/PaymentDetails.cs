c411fb01e7d976ba7f582d04c96fe1ffab0d052b
Adelin Petrisor Rau <82478810+adelinrau3@users.noreply.github.com>
2021-10-04 16:40:08 +0300

    Add files via upload

---
+++ RauAdelin/Lucrarea01_RauAdelin/Lucrarea01/Domain/PaymentDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lucrarea1_RauAdelin
+{
+    public record PaymentDetails(string PaymentAddress, int PaymentState) { }
+}
---
+++ RauAdelin/Lucrarea01_RauAdelin/Lucrarea01/Domain/UnvalidatedProduse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lucrarea1_RauAdelin
+{
+    public record UnvalidatedProduse(string ProdusID, string ProdusCantitate);
+
+}
