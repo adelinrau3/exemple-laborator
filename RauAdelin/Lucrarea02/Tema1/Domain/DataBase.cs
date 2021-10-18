40d7ff1516d122e35d5a465f45d2aa86f38e1d0f
Adelin Petrisor Rau <adelinrau3@gmail.com>
2021-10-18 22:48:15 +0300

    Lucrarea02

---
+++ RauAdelin/Lucrarea02/Tema1/Domain/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01.Fake
+{
+    public static class DataBase
+    {
+        public static IEnumerable<Product> LoadProducts()
+        {
+            return new List<Product>
+            {
+                new Product(1, "Prod1", 1.5f),
+                new Product(2, "Prod2", 2.5f),
+                new Product(3, "Prod3", 3.5f),
+                new Product(4, "Prod4", 4.5f),
+                new Product(5, "Prod5", 5.5f),
+            };
+        }
+    }
+}
